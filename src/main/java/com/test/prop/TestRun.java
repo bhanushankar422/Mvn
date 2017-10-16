716610a36e69d373351ba149fa76aa688ead1799
Pottipareddy, Bhanu S <Bhanu.S.Pottipareddy@HealthPartners.Com>
2017-10-16 16:42:29 -0500

    Added the pdf surefire plugin

--- src/main/java/com/test/prop/TestRun.java
+++ src/main/java/com/test/prop/TestRun.java
@@ -1,6 +1,9 @@
 package com.test.prop;
 
+import com.github.mkolisnyk.cucumber.reporting.CucumberConsolidatedReport;
 import com.github.mkolisnyk.cucumber.reporting.CucumberFeatureOverview;
+
+import java.io.File;
 
 public class TestRun {
     public static void main(String[] args){
@@ -12,6 +15,18 @@ public class TestRun {
         results.setSourceFile("./target/cucumber.json");
         try {
             results.execute(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        CucumberConsolidatedReport results1 = new CucumberConsolidatedReport();
+        results1.setOutputDirectory("target");
+        results1.setOutputName("cucumber-results");
+        results1.setPdfPageSize("A4 landscape");
+        results1.setSourceFile("./src/test/resources/cucumber.json");
+        try {
+            results1.execute(
+                    true
+            );
         } catch (Exception e) {
             e.printStackTrace();
         }
