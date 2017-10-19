1a23813e983c6f695e071d84319d78caf1cc2aa9
Pottipareddy, Bhanu S <Bhanu.S.Pottipareddy@HealthPartners.Com>
2017-10-19 14:48:26 -0500

    Added the pdf report generation

--- src/main/java/com/test/prop/TestRun.java
+++ src/main/java/com/test/prop/TestRun.java
@@ -1,6 +1,7 @@
 package com.test.prop;
 
 import com.github.mkolisnyk.cucumber.reporting.CucumberConsolidatedReport;
+import com.github.mkolisnyk.cucumber.reporting.CucumberDetailedResults;
 import com.github.mkolisnyk.cucumber.reporting.CucumberFeatureOverview;
 
 import java.io.File;
@@ -8,27 +9,6 @@ import java.io.File;
 public class TestRun {
     public static void main(String[] args){
         System.out.println(" In Test Run main");
-        CucumberFeatureOverview results = new CucumberFeatureOverview();
-        results.setOutputDirectory("target/reports");
-        results.setOutputName("cucumber-results");
 
-        results.setSourceFile("./target/cucumber.json");
-        try {
-            results.execute(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        CucumberConsolidatedReport results1 = new CucumberConsolidatedReport();
-        results1.setOutputDirectory("target");
-        results1.setOutputName("cucumber-results");
-        results1.setPdfPageSize("A4 landscape");
-        results1.setSourceFile("./src/test/resources/cucumber.json");
-        try {
-            results1.execute(
-                    true
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/test/java/com/test/prop/RunCukesTest.java
+++ src/test/java/com/test/prop/RunCukesTest.java
@@ -1,22 +1,24 @@
 package com.test.prop;
 
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import com.github.mkolisnyk.cucumber.runner.ExtendedTestNGRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
 @ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
         retryCount = 3,
         detailedReport = true,
+        detailedAggregatedReport = true,
         overviewReport = true,
         coverageReport = true,
         jsonUsageReport = "target/cucumber-usage.json",
         usageReport = true,
         toPDF = true,
-        outputFolder = "target")
+        pdfPageSize = "A3 portrait",
+        outputFolder = "target/reports")
 @CucumberOptions(plugin = { "html:target/cucumber-html-report",
         "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
         "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" })
-public class RunCukesTest {
+public class RunCukesTest extends ExtendedTestNGRunner {
 }
