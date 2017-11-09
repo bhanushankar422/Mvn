78c0ca6604bd7d6e05a05571b2ed3cc6279975d0
Pottipareddy, Bhanu S <Bhanu.S.Pottipareddy@HealthPartners.Com>
2017-11-09 15:17:53 -0600

    Updated the pom versions

--- src/main/java/com/test/prop/TestRun.java
+++ src/main/java/com/test/prop/TestRun.java
@@ -1,14 +1,7 @@
 package com.test.prop;
 
-import com.github.mkolisnyk.cucumber.reporting.CucumberConsolidatedReport;
-import com.github.mkolisnyk.cucumber.reporting.CucumberDetailedResults;
-import com.github.mkolisnyk.cucumber.reporting.CucumberFeatureOverview;
-
-import java.io.File;
-
 public class TestRun {
     public static void main(String[] args){
         System.out.println(" In Test Run main");
-
     }
 }
