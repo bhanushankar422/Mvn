1d00c8fb39781011456588a37ebf1363a3cc6979
Bhanu Shankar Pottipareddy <bhanushankar422@gmail.com>
2017-11-10 10:17:47 -0600

    Added sysout and updated surefire version

--- src/main/java/com/test/prop/Belly.java
+++ src/main/java/com/test/prop/Belly.java
@@ -6,14 +6,14 @@ import java.util.Properties;
 
 public class Belly {
     public void eat(int cukes) {
-        //System.out.println("In Belly eat");
+        System.out.println("In Belly eat");
         String resourceName = "app.properties"; // could also be a constant
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         Properties props = new Properties();
         try {
             try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
                 props.load(resourceStream);
-                //System.out.println(props.getProperty("name"));
+                System.out.println(props.getProperty("name"));
             }
         } catch (IOException e) {
             e.printStackTrace();
