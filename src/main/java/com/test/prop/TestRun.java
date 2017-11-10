613a92e1dc9d3212f446b99353ab1665bbfc20e6
Bhanu Shankar Pottipareddy <bhanushankar422@gmail.com>
2017-11-10 10:10:07 -0600

    Removed sysout

--- src/main/java/com/test/prop/TestRun.java
+++ src/main/java/com/test/prop/TestRun.java
@@ -2,6 +2,6 @@ package com.test.prop;
 
 public class TestRun {
     public static void main(String[] args){
-        System.out.println(" In Test Run main");
+        //System.out.println(" In Test Run main");
     }
 }
